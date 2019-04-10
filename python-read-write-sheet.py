60760eb717bdb1004f590e5dc36c32affaea5ff3
Casey DuBose <casey@dubose.haus>
2019-04-10 15:43:33 -0700

    Updating format to better comply with PEP8 and simplify redundant formulas.

--- python-read-write-sheet.py
+++ python-read-write-sheet.py
@@ -11,6 +11,7 @@ _dir = os.path.dirname(os.path.abspath(__file__))
 # The API identifies columns by Id, but it's more convenient to refer to column names. Store a map here
 column_map = {}
 
+
 # Helper function to find cell in a row
 def get_cell_by_column_name(row, column_name):
     column_id = column_map[column_name]
@@ -25,25 +26,24 @@ def evaluate_row_and_build_updates(source_row):
     # Find the cell and value we want to evaluate
     status_cell = get_cell_by_column_name(source_row, "Status")
     status_value = status_cell.display_value
-    if (status_value == "Complete"):
+    if status_value == "Complete":
         remaining_cell = get_cell_by_column_name(source_row, "Remaining")
-        if (remaining_cell.display_value != "0"):                           # Skip if already 0
+        if remaining_cell.display_value != "0":  # Skip if already 0
             print("Need to update row #" + str(source_row.row_number))
 
             # Build new cell value
-            newCell = smart.models.Cell()
-            newCell.column_id = column_map["Remaining"]
-            newCell.value = 0
+            new_cell = smart.models.Cell()
+            new_cell.column_id = column_map["Remaining"]
+            new_cell.value = 0
 
             # Build the row to update
-            newRow = smart.models.Row()
-            newRow.id = source_row.id
-            newRow.cells.append(newCell)
+            new_row = smart.models.Row()
+            new_row.id = source_row.id
+            new_row.cells.append(new_cell)
 
-            return newRow
+            return new_row
 
     return None
-
 
 
 print("Starting ...")
@@ -62,7 +62,7 @@ result = smart.Sheets.import_xlsx_sheet(_dir + '/Sample Sheet.xlsx', header_row_
 # Load entire sheet
 sheet = smart.Sheets.get_sheet(result.data.id)
 
-print ("Loaded " + str(len(sheet.rows)) + " rows from sheet: " + sheet.name)
+print("Loaded " + str(len(sheet.rows)) + " rows from sheet: " + sheet.name)
 
 # Build column map for later reference - translates column names to column id
 for column in sheet.columns:
@@ -73,7 +73,7 @@ rowsToUpdate = []
 
 for row in sheet.rows:
     rowToUpdate = evaluate_row_and_build_updates(row)
-    if (rowToUpdate != None):
+    if rowToUpdate is not None:
         rowsToUpdate.append(rowToUpdate)
 
 # Finally, write updated cells back to Smartsheet
@@ -82,9 +82,5 @@ if rowsToUpdate:
     result = smart.Sheets.update_rows(result.data.id, rowsToUpdate)
 else:
     print("No updates required")
-        
-print ("Done")
 
-
-
-
+print("Done")
